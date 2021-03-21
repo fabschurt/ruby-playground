aacf5fbc4d4091ac2d1904caaffdeba207d901ff
Fabien Schurter <fabien@fabschurt.com>
2021-03-21 21:48:51 +0100

    refactor: Streamline entity constructor

--- main.rb
+++ main.rb
@@ -8,43 +8,52 @@ $LOAD_PATH.prepend(
 require 'date'
 
 class Book
-  attr_accessor :title, :author_name, :publication_year
+  attr_reader :title
+  attr_reader :author_name
+  attr_reader :publication_year
 
-  def initialize(title:, author_name:, publication_year:)
-    validate(
-      title: title,
-      author_name: author_name,
-      publication_year: publication_year
-    )
-
-    @title = title
-    @author_name = author_name
-    @publication_year = publication_year
+  def initialize(title:, author_name:, publication_date:)
+    @title,
+    @author_name,
+    @publication_year,
+      = validate(title, author_name, publication_date)
   end
 
   private
 
-    def validate(title:, author_name:, publication_year:)
-      if
-        ! title.is_a? String ||
-        title.is_empty?
-      then fail ArgumentError, 'Title must be a non-empty string.' end
-
-      if
-        ! author_name.is_a? String ||
-        author_name.is_empty?
-      then fail ArgumentError, 'Author name must be a non-empty string.' end
-
-      if
-        ! publication_year.is_a? Date
-      then fail ArgumentError, 'Publication year must be a date' end
+  def validate(title, author_name, publication_date)
+    if
+      !(title.is_a? String) ||
+      title.empty?
+    then
+      fail ArgumentError, 'Title must be a non-empty string.'
     end
+
+    if
+      !(author_name.is_a? String) ||
+      author_name.empty?
+    then
+      fail ArgumentError, 'Author name must be a non-empty string.'
+    end
+
+    if
+      !(publication_date.is_a? Date)
+    then
+      fail ArgumentError, 'Publication year must be a date'
+    end
+
+    [
+      title,
+      author_name,
+      publication_date.year
+    ]
+  end
 end
 
 moby_d = Book.new(
-  publication_year: Date.new(1810, 10, 30),
   title: 'Moby D.',
-  author_name: 'Herman Melville'
+  author_name: 'Herman Melville',
+  publication_date: Date.new(1810, 10, 30),
 )
 
 pp moby_d
