11fda3da0705d85bf8d565131bb6e0f9631acfc8
Fabien Schurter <fabien@fabschurt.com>
2021-03-27 15:55:43 +0100

    style: Use a real name for a function

--- src/blocks_and_procs.rb
+++ src/blocks_and_procs.rb
@@ -1,9 +1,9 @@
 require 'minitest/autorun'
 
-def go_crazy(&block) = block_given? ? block.class : nil
+def get_block_class(&block) = block_given? ? block.class : nil
 
 describe 'Blocks and procs' do
   it 'is the same' do
-    _(go_crazy { nil }).must_be_same_as Proc
+    _(get_block_class { nil }).must_be_same_as Proc
   end
 end
