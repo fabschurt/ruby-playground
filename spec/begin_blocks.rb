bb9c569f6c3b07bfe88ff24fa5599e132784f696
Fabien Schurter <8007715+fabschurt@users.noreply.github.com>
2021-08-09 15:16:40 +0200

    test: Add "begin" blocks spec (#15)

--- spec/begin_blocks.rb
+++ spec/begin_blocks.rb
@@ -1,1 +1,26 @@
-# WIP
+require 'minitest/autorun'
+
+describe 'A «begin» block' do
+  it 'can encapsulate a procedure' do
+    what_the_fox_says = begin
+      fox_words = []
+      fox_words << 'Tiling tiling!'
+      fox_words << 'Cuh cuh cuh cuh cuh cuh!'
+    end
+
+    expect(what_the_fox_says).must_equal [
+      'Tiling tiling!',
+      'Cuh cuh cuh cuh cuh cuh!',
+    ]
+  end
+
+  it 'has the same semantics than a «def» block' do
+    def explode
+      fail 'BOOM!'
+    rescue
+      'This is fine.'
+    end
+
+    expect(explode).must_equal 'This is fine.'
+  end
+end
